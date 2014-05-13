2419cc8c54387ccf6c031a312fce42a5791cf8bf
Jason <masterjaso@gmail.com>
2014-05-13 14:18:23 -0400

    Added functionality to skeleton functions
    Implemented and tested working functions.

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,32 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	m <- NULL
+	set <- function(y) {
+		x <<- y
+		m <<- NULL
+	}
+	get <- function() x
+	setmat <- function(matrix) m <<- solve
+	getmat <- function() m
+	list(set = set, get = get,
+		 setmat = setmat,
+		 getmat = getmat)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+     m <- x$getmat()
+	 
+	if(!is.null(m)) {
+		message("getting cached data")
+		return(m)
+	}
+	
+	data <- x$get()
+	m <- solve(data, ...)
+	x$setmat(m)
+	m
 }
